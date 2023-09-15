034b8b7f74eeaa1b20ff92452483a95327d6c04e
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2023-09-15 18:27:33 -0300

    Footer, Paginação, busca
    Footer com img
    inicio da paginação
    busca com filtro com nome

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
-// const characters = '/character';
-// const locations = '/location';
-// const episodes = '/episode';
+const charactersContainer = document.querySelector(".chars-container");
+const searchCharacterByName = document.getElementById("searchCharacter");
+// const pagination = document.getElementById("pagination");
+const btnNext = document.querySelector(".btn-rickNext");
+const btnPrev = document.querySelector(".btn-rickPrev");
+const searchCharacters = document.getElementById("searchCharacter");
+let currentPage = 1;
 
 
-const charactersContainer = document.querySelector('.chars-container');
-const defaultFilters = {
-    name: '',
-    species: '',
-    gender: '',
-    status: '',
-    page:1
+async function getCharacters(page, name = "") {
+  try {
+    const params = { page, name };
+    const response = await api.get(`/character`, { params });
+
+    const characters = response.data.results;
+    const infos = response.data.info;
+    
+    charactersRickMorty(characters);
+    displayPaginationNext(infos);
+
+    console.log(infos);
+  } catch (error) {
+    console.log("Erro ao buscar", error);
+  }
 }
 
-async function getCharacters(){
-   
-    try {
-        const response = await api.get(`/character`);
-
-        const characters = response.data.results
-        console.log(characters);
-
-        characters.forEach(character => {
-            const cardCharacters = document.createElement('div');
-            cardCharacters.classList.add('card')
-            cardCharacters.innerHTML = `<img
+function charactersRickMorty(characters) {
+  charactersContainer.innerHTML = "";
+  characters.forEach((character) => {
+    const cardCharacters = document.createElement("div");
+    cardCharacters.classList.add("card");
+    cardCharacters.innerHTML = `<img
             src="${character.image}"
             alt="perfil"
           />
           <h2>${character.name}</h2>
           <p>${character.status}</p>
-          <p>${character.species}</p>`
-          charactersContainer.appendChild(cardCharacters)
-
-        })
-
-        
-    
-    }
-    catch (error) {console.log('Erro ao buscar',error);}
-
-    
+          <p>${character.species}</p>`;
+    charactersContainer.appendChild(cardCharacters);
+  });
 }
 
+  
 
-getCharacters()
-// getCharacters(defaultFilters)
+  
+ function displayPaginationNext(info) {
+     btnPrev.addEventListener("click",  async () => {
+    if (currentPage >= 2) {
+      currentPage-=1;
+      
+      await getCharacters(currentPage);
+    }
+  });
+
+   btnNext.addEventListener("click",  async () => {
+    if (currentPage < info.pages) {
+      currentPage+= 1;
+      
+      await getCharacters(currentPage);
+    }
+  });
+}
+
+searchCharacters.addEventListener("input", () => {
+  currentPage = 1;
+  getCharacters(currentPage, searchCharacters.value);
+});
+
+getCharacters();
